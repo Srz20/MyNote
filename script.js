84a7fabf6ef721983e57ec96a7f9c9cb2ee4e054
Srz20 <104067855+Srz20@users.noreply.github.com>
2023-01-24 02:18:19 +0530

    About my all notes

---
+++ script.js
@@ -0,0 +1,21 @@
+const cs1= document.querySelector('.cs')
+fetch('css.html').then(res=>res.text()).then(data=>{
+    cs1.innerHTML=data
+})
+
+
+const JavaScript= document.querySelector('.js')
+fetch('JavaScript.html').then(res=>res.text()).then(data=>{
+    JavaScript.innerHTML=data
+})
+
+const jQuery= document.querySelector('.jq')
+fetch('jQuery.html').then(res=>res.text()).then(data=>{
+    jQuery.innerHTML=data
+})
+
+
+const React= document.querySelector('.rt')
+fetch('React.html').then(res=>res.text()).then(data=>{
+    React.innerHTML=data
+})
